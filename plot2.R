6993d611063cc094e4af29a453139bb4c22bc04a
Ambrosio Torres Galvis <atorresgalvis@gmail.com>
2014-06-05 13:43:26 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,9 @@
+a<-read.table("household_power_consumption.txt", header=T, sep = ";")  ##read the data from the file in my working directory
+s<-split(a, a$Date)  ##split the data by Date to obtain data to each day
+d1<-s["1/2/2007"]; d2<-s["2/2/2007"]  ## obtain only the data to February the first and second of 2007
+d1 <- as.matrix(d1[[1]]) ; d2 <- as.matrix(d2[[1]])  ##convert data as a matrix
+dcomp <- rbind(d1, d2); dcomp <-data.frame(dcomp)  ##put together both, 1/2/2007 and 2/2/2007 data in one object called dcomp as data frame
+v1<-as.numeric(as.vector(dcomp$Global_active_power))  ##subset the variable "Global Active Power" from the data frame 
+png(file="plot2.png", width = 480, height = 480, units = "px")  ##open de device of PNG format
+plot(v1, type = "l") ##build a plot type lines
+dev.off() #"close de png device
